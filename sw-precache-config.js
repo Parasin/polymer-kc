faf030c90771feb360f07eaf8d00cfe598124f43
eevabec <evan.bechtol@ericsson.com>
2017-10-18 16:31:41 -0500

    Add service worker and update manifest

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -9,12 +9,49 @@
  */
 
 /* eslint-env node */
-
 module.exports = {
   staticFileGlobs: [
-    'index.html',
+    '/index.html',
     'manifest.json',
-    'bower_components/webcomponentsjs/*',
+    'bower_components/webcomponentsjs/webcomponents-loader.js',
+    'bower_components/polymer/*',
+    '/src/my-app.html',
+    '/src/my-kc.html',
+    '/src/my-survey-view.html',
+    '/src/my-survey-grid.html',
+    '/src/submission-dialog.html',
+    '/src/shared-styles.html'
   ],
   navigateFallback: 'index.html',
+  runtimeCaching: [
+    {
+      urlPattern: /\/bower_components\/webcomponentsjs\/.*.js/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          name: 'webcomponentsjs-polyfills-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/images\/.*/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
 };
