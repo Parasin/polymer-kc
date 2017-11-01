7f00d633698928c0e4fde91f9f75ca795b98de83
eevabec <evan.bechtol@ericsson.com>
2017-11-01 14:10:35 -0500

    Remove "auto" attribute on submission-dialog

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,14 +1,3 @@
-/**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
-
-/* eslint-env node */
 module.exports = {
   staticFileGlobs: [
     '/index.html',
@@ -34,22 +23,12 @@ module.exports = {
       }
     },
     {
-      urlPattern: /\/data\/images\/.*/,
+      urlPattern: /\/src\/*.*/,
       handler: 'cacheFirst',
       options: {
         cache: {
           maxEntries: 200,
           name: 'items-cache'
-        }
-      }
-    },
-    {
-      urlPattern: /\/data\/.*json/,
-      handler: 'fastest',
-      options: {
-        cache: {
-          maxEntries: 100,
-          name: 'data-cache'
         }
       }
     }
